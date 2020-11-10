a6bd9fe09d8a4017ba1254270cd9e960cfb2aea8
Purnank Jain <purnank.triceria@gmail.com>
2020-11-10 23:18:27 +0530

    [Purnank] | Adds Role cards for users

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,18 @@
+import { Grid } from '@material-ui/core';
 import React from 'react'
+import UserRoleCard from '../components/UserRoleCard';
 import withNavbar from './../hoc/Navbar';
 
 const Home = () => {
   return (
-    <div>
-      Hello World
-    </div>
+    <Grid container justify="space-evenly">
+      <Grid item xs={12} sm={5} lg={4}>
+        <UserRoleCard text="Admin" onClick={() => { }} />
+      </Grid>
+      <Grid item xs={12} sm={5} lg={4}>
+        <UserRoleCard text="User" onClick={() => { }} />
+      </Grid>
+    </Grid>
   )
 }
 
