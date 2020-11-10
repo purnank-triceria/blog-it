1284832d5ab4a2526d851208f1b9fe5ff831cd8b
Purnank Jain <purnank.triceria@gmail.com>
2020-11-11 00:12:25 +0530

    [Purnank] | Adds navigation to rolesCards

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,28 @@
 import { Grid } from '@material-ui/core';
 import React from 'react'
+import { useHistory } from 'react-router-dom';
 import UserRoleCard from '../components/UserRoleCard';
 import withNavbar from './../hoc/Navbar';
 
 const Home = () => {
+  const history = useHistory()
+  const handleAdminClick = () => {
+    history.push('/posts', {
+      role: 'admin'
+    })
+  }
+  const handleUserClick = () => {
+    history.push('/posts', {
+      role: 'user'
+    })
+  }
   return (
     <Grid container justify="space-evenly">
       <Grid item xs={12} sm={5} lg={4}>
-        <UserRoleCard text="Admin" onClick={() => { }} />
+        <UserRoleCard text="Admin" onClick={handleAdminClick} />
       </Grid>
       <Grid item xs={12} sm={5} lg={4}>
-        <UserRoleCard text="User" onClick={() => { }} />
+        <UserRoleCard text="User" onClick={handleUserClick} />
       </Grid>
     </Grid>
   )
