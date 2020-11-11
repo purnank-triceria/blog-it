f8d3331e533ee656a319472124641f54e4ee361f
purnank-triceria <purnank.triceria@gmail.com>
2020-11-11 22:36:11 +0530

    Interim

--- src/pages/Post.js
+++ src/pages/Post.js
@@ -1,6 +1,6 @@
 import { Grid, makeStyles } from '@material-ui/core'
 import React from 'react'
-import { useParams } from 'react-router-dom'
+import { useLocation, useParams } from 'react-router-dom'
 import { POSTS } from '../data/posts'
 import withNavbar from '../hoc/Navbar'
 
@@ -15,8 +15,9 @@ const useStyles = makeStyles(() => ({
   }
 }))
 
-const PostImage = ({ image, style }) => {
+const PostImage = ({ image, style, editable }) => {
   const classes = useStyles()
+  const 
   return (
     <div className={classes.imageBox} style={style}>
       <img src={image} className={classes.image} />
@@ -24,16 +25,19 @@ const PostImage = ({ image, style }) => {
   )
 }
 
+
 const Post = () => {
   const params = useParams()
   const postId = params.postId
   const post = POSTS.find((post) => post.id === Number(postId))
+  const location = useLocation()
+  const editable = location.state.role && location.state.role === 'admin'
   const { title, content, image } = post
   return (
     <Grid container>
       <Grid item md={2} />
       <Grid item md={8}>
-        <PostImage image={image} style={{paddingTop: '30px',}} />
+        <PostImage image={image} style={{ paddingTop: '30px', }} editable={editable} />
         <h2>{title}</h2>
         <div>{content}</div>
       </Grid>
