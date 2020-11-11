3bc883d768924aa1ef8329abf3ad4eceab90036c
purnank-triceria <purnank.triceria@gmail.com>
2020-11-11 16:08:09 +0530

    [Purnank] | Shows Posts on posts page

--- src/pages/Posts.js
+++ src/pages/Posts.js
@@ -1,6 +1,23 @@
+import { Grid } from '@material-ui/core';
 import React from 'react'
+import { useLocation } from 'react-router-dom';
+import PostCard from '../components/PostCard';
+import { POSTS } from '../data/posts';
 import withNavbar from './../hoc/Navbar';
 const Posts = () => {
-  return 'Posts'
+  const location = useLocation()
+  const role = location.state?.role || 'user'
+  const renderPosts = POSTS.map((post) =>
+    <Grid item md={4}>
+      <PostCard title={post.title} content={post.content} image={post.image} />
+    </Grid>
+  )
+  return (
+    <div style={{ padding: '50px', }}>
+      <Grid container spacing={5}>
+        {renderPosts}
+      </Grid>
+    </div>
+  )
 }
 export default withNavbar(Posts)
